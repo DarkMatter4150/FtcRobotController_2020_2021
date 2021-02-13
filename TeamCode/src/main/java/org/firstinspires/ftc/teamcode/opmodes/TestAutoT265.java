d1740a4d226e6be0a3742fa2f5f1143aad586939
BuildTools <unconfigured@null.spigotmc.org>
2021-02-13 10:03:57 -0600

    New Encoders Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAutoT265.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAutoT265.java
@@ -10,11 +10,16 @@ import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Transform2d;
 import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.qualcomm.robotcore.util.Range;
 import com.spartronics4915.lib.T265Camera;
 
+import org.firstinspires.ftc.teamcode.coyote.geometry.Pose;
+import org.firstinspires.ftc.teamcode.coyote.path.Path;
 import org.firstinspires.ftc.teamcode.drivecontrol.Robot;
 import org.firstinspires.ftc.teamcode.drivecontrol.RobotUtil;
 import org.firstinspires.ftc.teamcode.drivecontrol.Vector2d;
+
+import java.util.Vector;
 
 import static org.firstinspires.ftc.teamcode.drivecontrol.DriveModule.RotateModuleMode.DO_NOT_ROTATE_MODULES;
 import static org.firstinspires.ftc.teamcode.drivecontrol.DriveModule.RotateModuleMode.ROTATE_MODULES;
@@ -35,6 +40,8 @@ public class TestAutoT265 extends LinearOpMode {
     double currentX = 0;
     double currentY = 0;
     double currentTheta = 0;
+    Pose currentPose = new Pose(startingX, startingY, startingTheta);
+    public Path current_path;
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -48,6 +55,7 @@ public class TestAutoT265 extends LinearOpMode {
 
         //Actual OpMode
         driveToPosition(30,30,.5,true,false,2,this);
+
 
         while (opModeIsActive()) {
             updateSLAMNav();
@@ -205,11 +213,71 @@ public class TestAutoT265 extends LinearOpMode {
         currentX = startingX + -translation.getY();
         currentY = startingY + translation.getX();
         currentTheta = startingTheta + rotation.getDegrees();
+        currentPose = new Pose(currentX, currentY, currentTheta);
 
         telemetry.addData("X", currentX);
         telemetry.addData("Y", currentY);
         telemetry.addData("Rotation", currentTheta);
         telemetry.update();
     }
+
+
+    public void followCurvePath(Path path, LinearOpMode linearOpMode) {
+        this.current_path = path;
+
+
+        while (linearOpMode.opModeIsActive()) {
+
+            updateSLAMNav();
+            robot.updateBulkData();
+
+            // Get our lookahead point
+            path.update(currentPose);
+            Pose lookahead_pose = path.getFollowPose();
+
+            // Get the distance to our lookahead point
+            double distance = Math.hypot(lookahead_pose.x-currentPose.x, lookahead_pose.y-currentPose.y);
+ 
+            double speed;
+            // Find our drive speed based on distance
+            if (distance < current_path.getFollowCircle().radius) {
+                speed = Range.clip((distance / 8) + 0.1, 0, 1);
+            } else {
+                speed = 1;
+            }
+
+            // Find our turn speed based on angle difference
+            double turn_speed = Range.clip(Math.abs(currentPose.angle -lookahead_pose.angle) / (Math.PI/4) + 0.1, 0, 1);
+
+            // Drive towards the lookahead point
+            driveUsingPurePursuit(lookahead_pose, speed, turn_speed, linearOpMode);
+        }
+
+    }
+
+    public void driveUsingPurePursuit(Pose pose, double drive_speed, double turn_speed, LinearOpMode linearOpMode) {
+
+        // Find the angle to the pose
+        Vector2d directionPP = new Vector2d((pose.x-currentPose.x), (pose.y-currentPose.y)).scale(drive_speed);
+
+        // Find movement vector to drive towards that point
+        double mvmt_a = -Math.signum(Range.clip((pose.angle - currentPose.angle - (Math.PI/2)), -1, 1)) * turn_speed;
+
+        // Update actual motor powers with our movement vector
+
+        robot.updateBulkData();
+        updateSLAMNav();
+        robot.driveController.updateTracking();
+        linearOpMode.telemetry.addData("Driving robot", "");
+        linearOpMode.telemetry.addData("Current X: ", currentX);
+        linearOpMode.telemetry.addData("Current Y: ", currentY);
+        linearOpMode.telemetry.addData("Current Theta: ", currentTheta);
+        linearOpMode.telemetry.update();
+
+        robot.driveController.updatePositionTracking(telemetry); //update position tracking
+        robot.driveController.update(directionPP, mvmt_a);
+
+    }
+
 
 }
